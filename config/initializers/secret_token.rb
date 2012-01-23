c232984b675b6b8e2ebd95aac48f872add0a1bfc
Pau Compte <pausandalio@gmail.com>
2012-01-23 18:17:59 +0100

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manosalaobra::Application.config.secret_token = '4e25e3e701e568f87884d176486c7696b6d654f7ba5874fae25e49a9109bef8a53dc1958953a48673ad594cc8833a9ee1672733e4cd98f783828cd0baa63d30b'
