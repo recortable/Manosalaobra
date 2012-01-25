04fe17cbc206d637725c7465c46830dd886f214a
Pau Compte <pausandalio@gmail.com>
2012-01-25 03:28:16 +0100

    Añadida confirmación de password i canviados los labels que salían por defecto en el formulario para nevo usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ActiveRecord::Base
                    :length => {:minimum => 3, :maximum => 60},
                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
   validates :password, :presence => true,
-                       :length => {:minimum => 3, :maximum => 60}
+                       :length => {:minimum => 3, :maximum => 60},
+                       :confirmation => true
+  validates :password_confirmation, :presence => true
 end
