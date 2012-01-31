3e83a9645c63ee5abaf2a5c29797321501c33320
danigb <danigb@gmail.com>
2012-01-31 14:11:01 +0100

    Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
   respond_to :html, :json, :xml
+
+  # Hay que poner el prural para que funcione el singular
+  expose(:comments)
   expose(:comment)
-  
-  def create    
+
+  def create
     flash[:notice] = "Gracias por tu comentario!" if comment.save
-    
     respond_with comment, location: comment.post
   end
 end
