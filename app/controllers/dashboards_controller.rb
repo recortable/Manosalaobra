8fe737f0583b75fb019b939669aa142987e6c409
danigb <danigb@gmail.com>
2012-02-21 13:26:54 +0100

    Un solo layout. Quitar la version de ruby del rvm

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,13 +2,9 @@ class DashboardsController < ApplicationController
   expose(:posts) { Post.order('id ASC').limit(5) }
   expose(:page) { Page.where(id: 1).first || Page.new }
 
-  layout "wip_layout", only: :wip
-
   def show
-
   end
 
-  # WIP: work in progress
   def wip
   end
 end
