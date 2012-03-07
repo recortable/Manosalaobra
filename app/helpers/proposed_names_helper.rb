1380352fd8ef984e5fdb26ba70cc3c71b54b77f7
danigb <danigb@gmail.com>
2012-03-07 10:30:24 +0100

    Contador de dias que faltan para la votación

--- app/helpers/proposed_names_helper.rb
+++ app/helpers/proposed_names_helper.rb
@@ -1,12 +1,17 @@
 module ProposedNamesHelper
 
-  def process_name(name)
-    max_length = 15
+  def end_of_voting_in_days
+    today = Date.today
+    ends = Date.new(2012, 3, 17)
+    ends.mjd - today.mjd
+  end
+
+  MAX_LENGTH = 18
+  def divide_name(name, max_length = MAX_LENGTH)
     name.length > max_length ? divide(name) : name
   end
 
   private
-
   def divide(string)
     "#{h(string.slice(0, string.length/2))} <br /> #{h(string.slice(string.length/2, string.length))}".html_safe
   end
--- app/models/proposed_name.rb
+++ app/models/proposed_name.rb
@@ -1,6 +1,6 @@
 class ProposedName < ActiveRecord::Base
   # Relations
-  has_many :votes, dependent: :delete_all
+  has_many :votes, dependent: :delete_all, order: 'created_at DESC'
 
   # Validations
   validates :value, presence: true
